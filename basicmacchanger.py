d3a61e26ccf740d868e989cadd10f6e4e6f40b88
David (from Kali) <davidmitchellsimpson@gmail.com>
2024-06-25 19:37:29 -0400

    Changed part of the script to a function

--- basicmacchanger.py
+++ basicmacchanger.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
-#This script is designed to change the mac address of a network device on your linux computer.
 import subprocess
 import optparse
 
+#This script is designed to change the mac address of a network device on your linux computer.
+
+
+def get_arguments():
+    #Options for interface & new MAC address
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    return parser.parse_args()
+
 
 def change_mac(interface, new_mac):
-    print("[+] Changing MAC address for", interface, "to", new_mac)
-
+    #Brings the specified interface down, changes its MAC address, and brings it back up.
+    print("[+] Changing MAC address of", interface, "to", new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 
-parser = optparse.OptionParser()
-
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-
-(options, arguments) = parser.parse_args()
-
+(options, arguments) = get_arguments()
 change_mac(options.interface, options.new_mac)
